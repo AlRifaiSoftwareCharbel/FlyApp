097eb509e7dcf2e29790076cb4b7092064f1071d
Charbel Abi Khalil <36956102+AlRifaiSoftwareCharbel@users.noreply.github.com>
2019-03-01 09:30:51 +0200

    ss

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,30 @@ var myApp = angular.module('myApp', [
     'ngAnimate'
 ]);
 
+myApp.directive("ngTap", function() {
+    return function($scope, $element, $attributes) {
+      var tapped;
+      tapped = false;
+      $element.bind("click", function() {
+        if (!tapped) {
+          return $scope.$apply($attributes["ngTap"]);
+        }
+      });
+      $element.bind("touchstart", function(event) {
+        return tapped = true;
+      });
+      $element.bind("touchmove", function(event) {
+        tapped = false;
+        return event.stopImmediatePropagation();
+      });
+      return $element.bind("touchend", function() {
+        if (tapped) {
+          return $scope.$apply($attributes["ngTap"]);
+        }
+      });
+    };
+  });
+
 // Initialize the main module
 myApp.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
 
